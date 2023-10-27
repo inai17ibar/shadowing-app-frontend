52eaa7e675fa3e6a7f59feb52767900ca4f48289
Soichiro Inatani <inai17ibar@gmail.com>
2023-10-27 10:03:02 +0900

    Youtube動画検索の基本機能を追加。

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [videoIDs, setVideoIDs] = useState([]);
+
+  const searchVideos = async () => {
+    const response = await fetch(`http://localhost:8080/search?q=${query}`);
+    const data = await response.json();
+    setVideoIDs(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search YouTube"
+      />
+      <button onClick={searchVideos}>Search</button>
+
+      <ul>
+        {videoIDs.map((id) => (
+          <li key={id}>
+            <a href={`https://www.youtube.com/watch?v=${id}`} target="_blank" rel="noopener noreferrer">
+              Watch on YouTube
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
