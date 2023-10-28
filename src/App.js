54cd07e5c9e73736bfeb4ef5d7a4a57173c36f3f
Soichiro Inatani <inai17ibar@gmail.com>
2023-10-28 15:14:23 +0900

    タイトルwithURLとサムネイル画像を取得するフロントを実装。

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
+// App.js
+import React, { useState } from 'react';
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [videoIDs, setVideoIDs] = useState([]);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
-  const searchVideos = async () => {
-    const response = await fetch(`http://localhost:8080/search?q=${query}`);
-    const data = await response.json();
-    setVideoIDs(data);
-  };
+  const handleSearch = async () => {
+    fetch(`http://localhost:8080/search?q=${query}`)
+      .then(response => response.json())
+      .then(data => {
+        setResults(data.results);
+      })
+      .catch(error => {
+        console.error("Error fetching search results:", error);
+      });
+  }
 
   return (
     <div className="App">
-      <input
+      <input 
+        type="text" 
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search YouTube"
+        onChange={e => setQuery(e.target.value)}
       />
-      <button onClick={searchVideos}>Search</button>
-
-      <ul>
-        {videoIDs.map((id) => (
-          <li key={id}>
-            <a href={`https://www.youtube.com/watch?v=${id}`} target="_blank" rel="noopener noreferrer">
-              Watch on YouTube
-            </a>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleSearch}>Search</button>
+      {results.map((item, index) => (
+          <div key={index}>
+              <h3>
+                  <a href={`https://www.youtube.com/watch?v=${item.id}`} target="_blank" rel="noopener noreferrer">
+                      {item.title}
+                  </a>
+              </h3>
+              <img src={item.thumbnail} alt={item.title} />
+          </div>
+      ))}
     </div>
   );
 }
